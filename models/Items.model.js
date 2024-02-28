6b7718c478325f5afb2a727d61d9e0c7db8507c7
Eric <eric.coimbra@gmail.com>
2024-02-28 14:53:16 +0000

    add images to items

--- models/Items.model.js
+++ models/Items.model.js
@@ -3,6 +3,7 @@ const { Schema, model } = require('mongoose');
 const itemSchema = new Schema({
   name: { type: String, required: true, trim: true },
   effect: { type: String, required: true, trim: true },
+  image: String,
 });
 
 const Item = model('Item', itemSchema);
